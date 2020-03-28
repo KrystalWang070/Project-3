3c2070e26ab78a7c3c14f8dbb9eccd69cc0c2c3e
Alejandra Rosa <57076874+AlejandraRosa@users.noreply.github.com>
2020-03-28 14:58:26 -0500

    plot update

--- plots.js
+++ plots.js
@@ -1,24 +1,16 @@
 //https://plotly.com/javascript/time-series/
-
 Plotly.d3.csv("https://github.com/KrystalWang070/Project-3/blob/master/Resources/DOW.csv", function(err, rows){
-
   function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
 }
-
-
-//https://github.com/KrystalWang070/Project-3/blob/master/Resources/US%20Covid.csv
-
 // Create traces for different datasets
 var traceDow = {
-  type: "scatter",
-  mode: "lines",
+  type: 'scatter',
+  mode: 'lines',
   name: 'Dow & Covid-19 in USA',
   x: unpack(rows, 'Time'),
-  y: unpack(rows, 'Open'),
-  type: "bar"
+  y: unpack(rows, 'Open')
 };
-
 // var trace2 = {
 //   x: data.map(row => row.pair),
 //   y: data.map(row => row.romanSearchResults),
@@ -26,15 +18,11 @@ var traceDow = {
 //   name: "Roman",
 //   type: "bar"
 // };
-
 // Combining traces
 var data = [traceDow];
-
-// Apply the group barmode to the layout
 var layout = {
   title: "Dow & Covid-19 in USA"
 };
-
 // Render the plot to the div tag with id "plot"
 Plotly.newPlot('plot', data, layout);
 })
