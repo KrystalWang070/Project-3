db66763ef8fba2edeb085cf2d71c68ab8100b0ac
nalnajjar <57157097+nalnajjar@users.noreply.github.com>
2020-03-28 16:51:59 -0500

    Update plots.js

--- plots.js
+++ plots.js
@@ -61,38 +61,3 @@ Plotly.d3.csv("Resources/usa_combined.csv", function(buf){
 // // Render the plot to the div tag with id "plot"
 // Plotly.newPlot('plot', data, layout);
 // })
-
-Plotly.d3.csv("Resources/korea_combined.csv", function(buf){ 
-  var x_time = [], y_priceClose = [], y_covidKorea = [];
-  for (var i = 0; i < buf.length; i++) {
-    row = buf[i];
-    x_time.push( row['time'] );
-    y_priceClose.push( row['last'] );
-    y_covidKorea.push( row['cases'] );
-  }
-  var traceKOSPI = {
-    x: x_time,
-    y: y_priceClose,
-    mode: 'lines',
-    type: 'scatter',
-    name: 'KOSPI Close Price'
-  };
-  var traceCovidKorea = {
-    x: x_time,
-    y: y_covidKorea,
-    mode: 'lines',
-    type: 'scatter',
-    name: 'COVID-19 Reported Cases'
-  };
-
-  var data = [traceDow, traceCovidKorea];
-  var layout = {
-    yaxis: {
-       tickformat: '.0', 
-    title: "South Korea Covid-19 Cases & KOSPI Close Price",
-    xaxis: { title: "X-Axis" },
-    yaxis: { title: "Y-Axis"}
-    }
-  };
-  Plotly.newPlot('plot', data, layout);
-  } );
