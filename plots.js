c8a2a11f6698417bca840f2f7315ff94d2c6b855
Alejandra Rosa <57076874+AlejandraRosa@users.noreply.github.com>
2020-03-28 18:30:18 -0500

    combined plot file

--- plots.js
+++ plots.js
@@ -1,4 +1,5 @@
 //https://community.plotly.com/t/add-labels-to-time-series-read-from-csv/11586
+// USA plot
 Plotly.d3.csv("Resources/usa_combined.csv", function(buf){ 
   var x_time = [], y_priceClose = [], y_covidUS = [];
   for (var i = 0; i < buf.length; i++) {
@@ -33,31 +34,72 @@ Plotly.d3.csv("Resources/usa_combined.csv", function(buf){
   };
   Plotly.newPlot('plot', data, layout);
   } );
-// //https://plotly.com/javascript/time-series/
-// Plotly.d3.csv("https://github.com/KrystalWang070/Project-3/blob/master/Resources/DOW.csv", function(err, rows){
-//   function unpack(rows, key) {
-//   return rows.map(function(row) { return row[key]; });
-// }
-// // Create traces for different datasets
-// var traceDow = {
-//   type: 'scatter',
-//   mode: 'lines',
-//   name: 'Dow & Covid-19 in USA',
-//   x: unpack(rows, 'Time'),
-//   y: unpack(rows, 'Open')
-// };
-// // var trace2 = {
-// //   x: data.map(row => row.pair),
-// //   y: data.map(row => row.romanSearchResults),
-// //   text: data.map(row => row.romanName),
-// //   name: "Roman",
-// //   type: "bar"
-// // };
-// // Combining traces
-// var data = [traceDow];
-// var layout = {
-//   title: "Dow & Covid-19 in USA"
-// };
-// // Render the plot to the div tag with id "plot"
-// Plotly.newPlot('plot', data, layout);
-// })
+// Korea plot
+Plotly.d3.csv("Resources/korea_combined.csv", function(buf){ 
+  var x_time = [], y_priceClose = [], y_covidKorea = [];
+  for (var i = 0; i < buf.length; i++) {
+    row = buf[i];
+    x_time.push( row['time'] );
+    y_priceClose.push( row['last'] );
+    y_covidKorea.push( row['cases'] );
+  }
+  var traceKOSPI = {
+    x: x_time,
+    y: y_priceClose,
+    mode: 'lines',
+    type: 'scatter',
+    name: 'KOSPI Close Price'
+  };
+  var traceCovidKorea = {
+    x: x_time,
+    y: y_covidKorea,
+    mode: 'lines',
+    type: 'scatter',
+    name: 'COVID-19 Reported Cases'
+  };
+
+  var data = [traceDow, traceCovidKorea];
+  var layout = {
+    yaxis: {
+       tickformat: '.0', 
+    title: "South Korea Covid-19 Cases & KOSPI Close Price",
+    xaxis: { title: "X-Axis" },
+    yaxis: { title: "Y-Axis"}
+    }
+  };
+  Plotly.newPlot('plot', data, layout);
+});
+// UK plot
+Plotly.d3.csv("Resources/uk_combined.csv", function(buf){ 
+    var x_time = [], y_priceClose = [], y_covidUK = [];
+    for (var i = 0; i < buf.length; i++) {
+      row = buf[i];
+      x_time.push( row['time'] );
+      y_priceClose.push( row['last'] );
+      y_covidUK.push( row['cases'] );
+    }
+    var traceFTSE = {
+      x: x_time,
+      y: y_priceClose,
+      mode: 'lines',
+      type: 'scatter',
+      name: 'FTSE Close Price'
+    };
+    var traceCovidUK = {
+      x: x_time,
+      y: y_covidFTSE,
+      mode: 'lines',
+      type: 'scatter',
+      name: 'COVID-19 Reported Cases'
+    };
+    var data = [traceFTSE, traceCovidUK];
+    var layout = {
+      yaxis: {
+         tickformat: '.0', 
+      title: "United Kingdom Covid-19 Cases & FTSE Close Price",
+      xaxis: { title: "X-Axis" },
+      yaxis: { title: "Y-Axis"}
+      }
+    };
+    Plotly.newPlot('plot', data, layout);
+});
