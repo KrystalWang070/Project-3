0540dfff2337f9626a87a11366686e8e5ff49017
Alejandra Rosa <57076874+AlejandraRosa@users.noreply.github.com>
2020-03-28 14:36:42 -0500

    Update plots.js

--- plots.js
+++ plots.js
@@ -7,32 +7,34 @@ Plotly.d3.csv("https://github.com/KrystalWang070/Project-3/blob/master/Resources
 }
 
 
+//https://github.com/KrystalWang070/Project-3/blob/master/Resources/US%20Covid.csv
 
 // Create traces for different datasets
 var traceDow = {
-  x: data.map(row => row.pair),
-  y: data.map(row => row.greekSearchResults),
-  text: data.map(row => row.greekName),
-  name: "Dow & Covid-19",
+  type: "scatter",
+  mode: "lines",
+  name: 'Dow & Covid-19 in USA',
+  x: unpack(rows, 'Time'),
+  y: unpack(rows, 'Open'),
   type: "bar"
 };
 
-var trace2 = {
-  x: data.map(row => row.pair),
-  y: data.map(row => row.romanSearchResults),
-  text: data.map(row => row.romanName),
-  name: "Roman",
-  type: "bar"
-};
+// var trace2 = {
+//   x: data.map(row => row.pair),
+//   y: data.map(row => row.romanSearchResults),
+//   text: data.map(row => row.romanName),
+//   name: "Roman",
+//   type: "bar"
+// };
 
 // Combining traces
-var data = [trace1, trace2];
+var data = [traceDow];
 
 // Apply the group barmode to the layout
 var layout = {
-  title: "Greek vs Roman gods search results",
-  barmode: "group"
+  title: "Dow & Covid-19 in USA"
 };
 
 // Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+Plotly.newPlot('plot', data, layout);
+})
