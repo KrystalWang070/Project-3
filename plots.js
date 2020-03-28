d2907c68473daf5ff6b6210d0d7a51794c5a090a
Alejandra Rosa <57076874+AlejandraRosa@users.noreply.github.com>
2020-03-28 18:46:57 -0500

    Update plots.js

--- plots.js
+++ plots.js
@@ -35,29 +35,29 @@ Plotly.d3.csv("Resources/usa_combined.csv", function(buf){
   } );
 // Korea plot
 Plotly.d3.csv("Resources/korea_combined.csv", function(buf){ 
-  var x_time = [], y_priceClose = [], y_covidKorea = [];
+  var x_time1 = [], y_priceClose1 = [], y_covidKorea = [];
   for (var i = 0; i < buf.length; i++) {
     row = buf[i];
-    x_time.push( row['time'] );
-    y_priceClose.push( row['last'] );
+    x_time1.push( row['time'] );
+    y_priceClose1.push( row['last'] );
     y_covidKorea.push( row['cases'] );
   }
   var traceKOSPI = {
-    x: x_time,
-    y: y_priceClose,
+    x: x_time1,
+    y: y_priceClose1,
     mode: 'lines',
     type: 'scatter',
     name: 'KOSPI Close Price'
   };
   var traceCovidKorea = {
-    x: x_time,
+    x: x_time1,
     y: y_covidKorea,
     mode: 'lines',
     type: 'scatter',
     name: 'COVID-19 Reported Cases'
   };
 
-  var data = [traceDow, traceCovidKorea];
+  var data = [traceKOSPI, traceCovidKorea];
   var layout = {
     yaxis: {
        tickformat: '.0', 
@@ -70,22 +70,22 @@ Plotly.d3.csv("Resources/korea_combined.csv", function(buf){
 });
 // UK plot
 Plotly.d3.csv("Resources/uk_combined.csv", function(buf){ 
-    var x_time = [], y_priceClose = [], y_covidUK = [];
+    var x_time2 = [], y_priceClose2 = [], y_covidUK = [];
     for (var i = 0; i < buf.length; i++) {
       row = buf[i];
-      x_time.push( row['time'] );
-      y_priceClose.push( row['last'] );
+      x_time2.push( row['time'] );
+      y_priceClose2.push( row['last'] );
       y_covidUK.push( row['cases'] );
     }
     var traceFTSE = {
-      x: x_time,
-      y: y_priceClose,
+      x: x_time2,
+      y: y_priceClose2,
       mode: 'lines',
       type: 'scatter',
       name: 'FTSE Close Price'
     };
     var traceCovidUK = {
-      x: x_time,
+      x: x_time2,
       y: y_covidFTSE,
       mode: 'lines',
       type: 'scatter',
